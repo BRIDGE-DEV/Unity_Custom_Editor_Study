d524ca1f003719b7270002e05c28e5bd4ecd81a4
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-03-02 00:28:27 +0900

    Test Push

---
+++ Editor_SandBox/Packages/ReInputTool/Editor/ReInput.cs
@@ -0,0 +1,114 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.UIElements;
+
+public class ReInput : EditorWindow
+{
+    private const string fileName = "KeyInfoList.json";
+
+    private KeyInfoList keyInfoList;
+    private FileDataHandler fileDataHandler;
+
+    [MenuItem("Window/ReInput")]
+    public static void ShowWindow()
+    {
+        ReInput reInput = GetWindow<ReInput>("Custom Package - ReInput");
+        reInput.titleContent = new GUIContent("ReInput");
+    }
+
+    private void OnEnable()
+    {
+        fileDataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
+
+        keyInfoList = fileDataHandler.Load();
+        rootVisualElement.Add(CreateKeyInfoList());
+        
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/com.bridgedev.reinput/Editor/ReInput.uxml"); //Packages/com.unity.images-library
+        VisualElement labelFromUXML = visualTree.Instantiate();
+        rootVisualElement.Add(labelFromUXML);
+        
+        Button saveButton = rootVisualElement.Q<Button>("save-button");
+        saveButton.clicked += OnSaveButtonClicked;
+    }
+
+    private VisualElement CreateKeyInfoList()
+    {
+        var keyInfoListElement = new VisualElement();
+        keyInfoListElement.name = "keyInfo-list";
+
+        foreach (KeyInfo keyInfo in keyInfoList.keyInfos)
+        {
+            keyInfoListElement.Add(CreateKeyInfoFoldout(keyInfo));
+        }
+        
+        var foldOut = new Foldout();
+        foldOut.text = "Input Key List";
+        foldOut.value = false;
+        foldOut.contentContainer.Add(keyInfoListElement);
+
+        return foldOut;
+    }
+
+    private Foldout CreateKeyInfoFoldout(KeyInfo keyInfo)
+    {
+        var keyInfoElement = new VisualElement();
+        keyInfoElement.name = "keyInfo-element";
+        
+        var keyCodeField = new TextField("Key");
+        keyCodeField.value = keyInfo.key.ToString();
+        keyCodeField.RegisterValueChangedCallback(evt => {
+            var targetKeyInfo = keyInfoList.keyInfos.Find(info => info.time.Equals(keyInfo.time));
+            targetKeyInfo.key = (KeyCode)Enum.Parse(typeof(KeyCode), evt.newValue);
+        });
+
+        keyInfoElement.Add(keyCodeField);
+
+        var timeField = new FloatField("Time");
+        timeField.value = keyInfo.time;
+        timeField.RegisterValueChangedCallback(evt => {
+            var targetKeyInfo = keyInfoList.keyInfos.Find(info => info.time.Equals(keyInfo.time));
+            targetKeyInfo.time = evt.newValue;
+        });
+
+        keyInfoElement.Add(timeField);
+
+        var keyStatusField = new Toggle("KeyStatus");
+        keyStatusField.value = keyInfo.keyStatus;
+        keyStatusField.RegisterValueChangedCallback(evt => {
+            var targetKeyInfo = keyInfoList.keyInfos.Find(info => info.time.Equals(keyInfo.time));
+            targetKeyInfo.keyStatus = evt.newValue;
+        });
+
+        keyInfoElement.Add(keyStatusField);
+        
+        var foldout = new Foldout();
+
+        foldout.text = keyInfo.key.ToString();
+        foldout.value = true;
+        foldout.contentContainer.Add(keyInfoElement);
+
+        return foldout;
+    }
+
+    private void OnSaveButtonClicked()
+    {
+        try
+        {
+            keyInfoList.keyInfos.Sort(delegate(KeyInfo info1, KeyInfo info2)
+            {
+                return info1.time.CompareTo(info2.time);
+            });
+
+            fileDataHandler.Save(keyInfoList);
+            
+            Debug.Log("[ReInput] Save Success!");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+}
